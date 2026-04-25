e6a9d4343b8e14ba7260b6142a2372a2b056b2ed
fennerii <jonathanfennerii@gmail.com>
2026-04-24 23:13:52 -0400

    Built Academic Buildings

---
+++ src/views/Academic_Buildings.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="Academic_Buildings">
+        <h1>Academic Buildings</h1>
+        <p>This page will display information about the academic buildings on campus.</p>
+    </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+</style>
