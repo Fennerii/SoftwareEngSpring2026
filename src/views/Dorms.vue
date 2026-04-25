0d64f6f1dadd591c2b4de6da2eb4a3974e5f2896
fennerii <jonathanfennerii@gmail.com>
2026-04-24 23:39:09 -0400

    Built Dorms

---
+++ src/views/Dorms.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="Dorms">
+        <h1>Dormitories</h1>
+        <p>This page will display information about the dormitories on campus.</p>
+    </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+</style>
